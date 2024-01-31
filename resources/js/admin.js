88f6a70fc9f0da19069926bf668e4da94693672c
NOOBHECMETAHA <ebaltboupotha@gmail.com>
2024-01-31 20:39:34 +0300

    Clean up code

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue';
+import './plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css';
+import './plugins/icheck-bootstrap/icheck-bootstrap.min.css';
+import './plugins/jqvmap/jqvmap.min.css';
+import './dist/css/adminlte.min.css';
+import './plugins/overlayScrollbars/css/OverlayScrollbars.min.css';
+import './plugins/daterangepicker/daterangepicker.css';
+import './plugins/summernote/summernote-bs4.min.css';
 
+import { createApp } from 'vue';
 import MaterialsDataTableComponent from "./components/admin/Materital/MaterialsDataTableComponent.vue";
 
 const app = createApp({
